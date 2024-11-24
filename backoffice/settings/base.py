722bfda0a4d86f0f5705e3ba739b5aa4cb60d3cf
BDlhj <akk808@snu.ac.kr>
2024-11-25 01:02:46 +0900

    modify: Remove default DEBUG value from initializing env

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 import environ
 
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
