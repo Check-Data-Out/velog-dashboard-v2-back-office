ac30c7467784c154ea7e52692755cc98595e70ab
BDlhj <akk808@naver.com>
2024-11-29 02:59:32 +0900

    feature: Create users app and define model

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "corsheaders",
     "django_extensions",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
---
+++ users/models.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.db import models
+
+from users.utils import generate_random_group_id
+
+
+class User(models.Model):
+    velog_uuid = models.UUIDField(
+        default=uuid.uuid4, unique=True, verbose_name="사용자 UUID"
+    )
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    group_id = models.IntegerField(
+        default=generate_random_group_id(), verbose_name="그룹 ID"
+    )
+    email = models.EmailField(unique=True, verbose_name="이메일")
+    is_active = models.BooleanField(default=True, verbose_name="활성 여부")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="생성 일시"
+    )
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정 일시")
+
+    def __str__(self):
+        return f"{self.velog_uuid}"
+
+    class Meta:
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자 목록"
