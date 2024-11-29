9b4244b99d4255dc0c2ecf13bffb6376b11ccbb0
BDlhj <akk808@naver.com>
2024-11-30 01:58:36 +0900

    modify: Refactor admin page

--- posts/admin.py
+++ posts/admin.py
@@ -5,61 +5,57 @@ from django.utils.html import format_html
 from posts.models import Post, PostDailyStatistics, PostStatistics
 
 
+class BasePostRelatedAdmin(admin.ModelAdmin):
+    def post_link(self, obj):
+        url = reverse("admin:posts_post_change", args=[obj.post.id])
+        return format_html(
+            '<a href="{}" target="_blank" rel="noopener noreferrer" class="admin-link">확인</a>',
+            url,
+        )
+
+    post_link.short_description = "게시글 정보"
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         "post_uuid",
-        "get_user",
+        "user_link",
         "title",
         "created_at",
         "updated_at",
     ]
 
-    def get_user(self, obj):
+    def user_link(self, obj):
+        url = reverse("admin:users_user_change", args=[obj.user.id])
         return format_html(
             f'<a target="_blank" href="{{}}" style="min-width: 80px; display: block;">{obj.user.velog_uuid}</a>',
-            f"{reverse('admin:users_user_change', args=[obj.user.id])}",
+            url,
         )
 
-    get_user.short_description = "사용자"
+    user_link.short_description = "사용자"
 
 
 @admin.register(PostStatistics)
-class PostStatisticsAdmin(admin.ModelAdmin):
+class PostStatisticsAdmin(BasePostRelatedAdmin):
     list_display = [
         "post",
-        "get_post_info",
+        "post_link",
         "view_count",
         "like_count",
         "created_at",
         "updated_at",
     ]
 
-    def get_post_info(self, obj):
-        return format_html(
-            '<a target="_blank" href="{}" style="min-width: 80px; display: block;">확인</a>',
-            f"{reverse('admin:posts_post_change', args=[obj.post.id])}",
-        )
-
-    get_post_info.short_description = "게시글 정보"
-
 
 @admin.register(PostDailyStatistics)
-class PostDailyStatisticsAdmin(admin.ModelAdmin):
+class PostDailyStatisticsAdmin(BasePostRelatedAdmin):
     list_display = [
         "post",
-        "get_post_info",
+        "post_link",
         "date",
         "daily_view_count",
         "daily_like_count",
         "created_at",
         "updated_at",
     ]
-
-    def get_post_info(self, obj):
-        return format_html(
-            '<a target="_blank" href="{}" style="min-width: 80px; display: block;">확인</a>',
-            f"{reverse('admin:posts_post_change', args=[obj.post.id])}",
-        )
-
-    get_post_info.short_description = "게시글 정보"
