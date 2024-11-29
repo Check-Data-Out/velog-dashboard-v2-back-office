0c0932a042190ba71573b913df1ebcc3c02b770b
BDlhj <akk808@naver.com>
2024-11-30 00:55:37 +0900

    modify: Update User model to inherit from TimeStampedModel and modify admin

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "velog_uuid",
+        "access_token",
+        "refresh_token",
+        "group_id",
+        "email",
+        "is_active",
+        "created_at",
+        "updated_at",
+    ]
--- users/models.py
+++ users/models.py
@@ -1,25 +1,35 @@
-import uuid
-
 from django.db import models
 
+from common.models import TimeStampedModel
 from users.utils import generate_random_group_id
 
 
-class User(models.Model):
+class User(TimeStampedModel):
+    """
+    대시보드 사용자 모델
+    """
+
     velog_uuid = models.UUIDField(
-        default=uuid.uuid4, unique=True, verbose_name="사용자 UUID"
+        blank=False, null=True, unique=True, verbose_name="사용자 UUID"
     )
-    access_token = models.TextField()
-    refresh_token = models.TextField()
+    access_token = models.TextField(
+        blank=False, null=False, verbose_name="Access Token"
+    )
+    refresh_token = models.TextField(
+        blank=False, null=False, verbose_name="Refresh Token"
+    )
     group_id = models.IntegerField(
-        default=generate_random_group_id, verbose_name="그룹 ID"
+        blank=True,
+        null=False,
+        default=generate_random_group_id,
+        verbose_name="그룹 ID",
     )
-    email = models.EmailField(unique=True, verbose_name="이메일")
-    is_active = models.BooleanField(default=True, verbose_name="활성 여부")
-    created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name="생성 일시"
+    email = models.EmailField(
+        blank=False, null=False, unique=True, verbose_name="이메일"
     )
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정 일시")
+    is_active = models.BooleanField(
+        default=True, null=False, verbose_name="활성 여부"
+    )
 
     def __str__(self):
         return f"{self.velog_uuid}"
