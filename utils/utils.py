761cb10ab851dcd34f88ac545d860f194198760b
ooheunda <ooheunda@gmail.com>
2026-03-01 20:47:10 +0900

    modify: to_local_date docs 수정

--- utils/utils.py
+++ utils/utils.py
@@ -31,7 +31,7 @@ def get_local_now_date() -> datetime:
 
 
 def to_local_date(dt: datetime) -> datetime:
-    """django timezone 을 기반으로 하는 실제 local의 date로 변환"""
+    """datetime 을 django timezone 을 따르는 date 로 cating 하는 함수"""
     local_dt = timezone.localtime(dt, timezone=timezone.get_current_timezone())
     return local_dt.replace(hour=0, minute=0, second=0, microsecond=0)
 
